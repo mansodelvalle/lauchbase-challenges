0138eaa420bba65c305a1a65218a590db51551f2
Marcos Del Valle <agenciatomz@gmail.com>
2020-01-31 15:02:58 -0300

    Desafio 2.2 do módulo 2 - RocketSeat

--- modulo-1/desafio01.js
+++ modulo-1/desafio01.js
@@ -2,15 +2,15 @@
     DESAFIO 1 - PROGRAMA PARA CALCULAR O IMC E NÍVEL DE OBESIDADE DE UMA PESSOA 
    ============================================ */
 
-const nome = 'Marcos'
-const peso = 58
-const altura = 1.75
-const imc = peso / ( altura * altura )
+const name = 'Marcos'
+const weight = 58
+const height = 1.75
+const imc = weight / ( height * height )
 console.log(imc)
 
 if(imc >= 30 || imc === 30)
 {
-    console.log(`${nome} você está acima do peso`)
+    console.log(`${name} você está acima do peso`)
 }else{
-    console.log(`${nome} você não está acima do peso`)
+    console.log(`${name} você não está acima do peso`)
 }
--- modulo-1/desafio02.js
+++ modulo-1/desafio02.js
@@ -2,20 +2,20 @@
         DESAFIO 2 - Cálculo de aposentadoria
    ============================================ */
 
-   const nome = 'Silvana'
-   const sexo = 'M'
-   const idade = 48
-   const contribuicao = 23
-   const calc = (idade + contribuicao) 
+   const name = 'Silvana'
+   const genre = 'M'
+   const age = 48
+   const contribution = 23
+   const calc = (age + contribution) 
 
-   if(contribuicao >= 30 && sexo === 'F' || contribuicao >= 35 && sexo === 'M' )
+   if(contribution >= 30 && genre === 'F' || contribution >= 35 && genre === 'M' )
    {
-       if(calc >= 85 && sexo === 'F' || calc >= 95 && sexo === 'M')
+       if(calc >= 85 && genre === 'F' || calc >= 95 && genre === 'M')
        {
-           console.log(`${nome} você pode se aposentar`)
+           console.log(`${name} você pode se aposentar`)
        }else{
-           console.log(`${nome} você não pode se aposentar`)
+           console.log(`${name} você não pode se aposentar`)
        }
    }else{
-       console.log(`${nome}, você não pode se aposentar`)
+       console.log(`${name}, você não pode se aposentar`)
    }
